58608a2b7101982b2892399cc5f0f038ca1e86e8
cordeiroewerton <67035660+cordeiroewerton@users.noreply.github.com>
2022-06-21 14:34:05 -0300

    Update matrix.sv

--- matrix.sv
+++ matrix.sv
@@ -1,9 +1,9 @@
-module two_d_array;
-  /Declarando array 
-  int array[2:0][3:0];        //2 dimension array
- 
+module matrix_exemplo;
+  //Declarando array 
+  int array[2:0][3:0];
+  
   initial begin
-    /Definindo valores do array
+    //Definindo valores do array
     array = '{'{0,1,2,3},'{4,5,6,7},'{8,9,10,11}};
  
     //Imprimindo valores do array 
